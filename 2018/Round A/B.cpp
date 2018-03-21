dd9c9f937f0b4e4ef39a352913a1321d6a48036a
Elvis Yu-Jing Lin <elvisyjlin@gmail.com>
2018-03-22 01:46:15 +0800

    2018 Round A Prob.B Cpp

---
+++ 2018/Round A/B.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define repeat(i, n) for (int i = 0; (i) < (n); ++(i))
+using namespace std;
+
+inline bool isOdd(int i) { return i % 2 == 1; }
+
+double solve(int N, int K, vector<double> V) {
+	sort(V.begin(), V.end());
+	double sum = 0;
+	repeat(i, N) { sum += V[i]; }
+	double ek = sum / N;
+	repeat(i, K) {
+		vector<double>::iterator x = upper_bound(V.begin(), V.end(), ek);
+		ek = accumulate(x, V.end(), ek * (x - V.begin())) / N;
+	}
+	return ek;
+}
+
+int main(void) { _
+	int T; cin >> T;
+	repeat(t, T) {
+		int N, K; cin >> N >> K;
+		vector<double> V; repeat(i, N) { double v; cin >> v; V.push_back(v); }
+		double e = solve(N, K, V);
+		printf("Case #%d: %.6f\n", t+1, e);
+	}
+    return 0;
+}
